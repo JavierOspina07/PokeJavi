10cb41a348e9248b2076ebabb96cccf86f70cf4d
Javier Ospina <jaheosig10@gmail.com>
2023-07-31 23:05:14 -0500

    Se agrego un filtrador por tipo

--- src/pages/PokedexPage.jsx
+++ src/pages/PokedexPage.jsx
@@ -2,19 +2,23 @@ import { useSelector } from "react-redux";
 import useFetch from "../hooks/useFetch";
 import { useEffect, useRef, useState } from "react";
 import PokeCard from "../components/PokedexPage/PokeCard";
-import './styles/PokedexPage.css'
+import "./styles/PokedexPage.css";
+import SelectType from "../components/PokedexPage/SelectType";
 
 const PokedexPage = () => {
-  
   const [inputValue, setInputValue] = useState("");
+  const [selectValue, setSelectValue] = useState("allPokemons");
+
   const trainer = useSelector((reducer) => reducer.trainer);
-  
-  const url = "https://pokeapi.co/api/v2/pokemon?offset=0&limit=250";
-  const [pokemons, getAllPokemons] = useFetch(url);
+
+  const url = "https://pokeapi.co/api/v2/pokemon?offset=0&limit=251";
+  const [pokemons, getAllPokemons, getPokemonsByType] = useFetch(url);
 
   useEffect(() => {
-    getAllPokemons();
-  }, []);
+    selectValue === "allPokemons"
+      ? getAllPokemons()
+      : getPokemonsByType(selectValue);
+  }, [selectValue]);
 
   const inputSearch = useRef();
 
@@ -23,9 +27,7 @@ const PokedexPage = () => {
     setInputValue(inputSearch.current.value.trim().toLowerCase());
   };
 
-  const cbFilter = (poke) => (
-    poke.name.includes(inputValue) 
-  );
+  const cbFilter = (poke) => poke.name.includes(inputValue);
 
   return (
     <div>
@@ -36,6 +38,7 @@ const PokedexPage = () => {
         <input ref={inputSearch} type="text" />
         <button>Search</button>
       </form>
+      <SelectType setSelectValue={setSelectValue} />
       <div className="card-container">
         {pokemons?.results.filter(cbFilter).map((poke) => (
           <PokeCard key={poke.url} url={poke.url} />
