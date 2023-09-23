8a3cbe4f031bf03002984a757195e14181fae730
Javier Ospina <jaheosig10@gmail.com>
2023-09-22 20:31:49 -0500

    cambios

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,11 +1,10 @@
-import "../styles/Loader.css"
+import '../styles/Loader.css'
 
 const Loader = () => {
   return (
-    <div className="loader__countainer">
-      <div class="loader">
-        <div class="waves"></div>
-      </div>
+    <div className='loader__countainer'>
+      <span className='loader'></span>
+      <img className='loader_img' src="./img/pokeball.png" alt="Descripción de la imagen"></img>
     </div>
   )
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,9 @@
 import { useDispatch, useSelector } from "react-redux"
-import { useRef } from "react"
+import { useRef, useState } from "react"
 import { setTrainerG } from "../store/slices/trainer.slice"
 import { useNavigate } from "react-router-dom"
 import "./styles/HomePage.css"
+
 
 const HomePage = () => {
   const inputTrainer = useRef()
@@ -11,7 +12,8 @@ const HomePage = () => {
 
   const navigate = useNavigate()
 
-  const handleSubmit = e => {
+
+  const handleSubmit = async e => {
     e.preventDefault()
     //e.target.inputTrainer.value
     dispatch(setTrainerG(inputTrainer.current.value))
@@ -20,27 +22,31 @@ const HomePage = () => {
 
   return (
     <div className="home-container">
-      <div className="home">
-        <img className="home__img" src="/img/pokedextittle.png" alt="" />
-        <h2 className="home__title">¡Hi trainer!</h2>
-        <p className="home__description">
-          To get started with the application, give me your trainer name 😉.
-        </p>
-        <form className="home__form" onSubmit={handleSubmit}>
-          <input
-            className="home__form-input"
-            id="inputTrainer"
-            ref={inputTrainer}
-            type="text"
-            placeholder="trainer's name"
-          />
-          <button className="home__form-btn">Gotta catch'em all!</button>
-        </form>
-      </div>
-      <footer className="home__footer">
-        <div className="home__rectangle-red"></div>
-        <div className="home__rectangle-black"><img className="pokebola" src="/img/botonpokebola.svg" alt="" /></div>
-      </footer>
+        <>
+          <div className="home">
+            <img className="home__img" src="/img/pokedextittle.png" alt="" />
+            <h2 className="home__title">¡Hi trainer!</h2>
+            <p className="home__description">
+              To get started with the application, give me your trainer name 😉.
+            </p>
+            <form className="home__form" onSubmit={handleSubmit}>
+              <input
+                className="home__form-input"
+                id="inputTrainer"
+                ref={inputTrainer}
+                type="text"
+                placeholder="trainer's name"
+              />
+              <button className="home__form-btn">Gotta catch'em all!</button>
+            </form>
+          </div>
+          <footer className="home__footer">
+            <div className="home__rectangle-red"></div>
+            <div className="home__rectangle-black">
+              <img className="pokebola" src="/img/botonpokebola.svg" alt="" />
+            </div>
+          </footer>
+        </>
     </div>
   )
 }
